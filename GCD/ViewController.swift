664b55c08ccca698681409febb1fc1568cebe24c
DevBorisElkin <ea.foreverrr@gmail.com>
2022-07-11 22:04:28 +0300

    Initial commit

---
+++ GCD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GCD
+//
+//  Created by test on 11.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
