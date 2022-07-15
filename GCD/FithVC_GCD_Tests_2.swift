a47fbdb07d4797b273e03ad71dc16406450a17a3
DevBorisElkin <ea.foreverrr@gmail.com>
2022-07-16 00:12:12 +0300

    added links as an explanation

--- GCD/FithVC_GCD_Tests_2.swift
+++ GCD/FithVC_GCD_Tests_2.swift
@@ -14,6 +14,13 @@ class FithVC_GCD_Tests_2: UIViewController {
     // workItem.cancel() -> will stop execution of workItem, we can check if(!workItem.isCancelled){ workItem.cancel() }
     
     // FOR SOME REASON WORKITEM.CANCEL() DOES NOT WORK!
+    // https://medium.com/@yostane/swift-sweet-bits-the-dispatch-framework-ios-10-e34451d59a86
+    // .cancel() will cancel something that has not been invoked, but not something that is running, we need to make a workaround and force the code that's being executed to stop working
+    // https://overcoder.net/q/835089/dispatchworkitem-%D0%BD%D0%B5-%D0%B7%D0%B0%D0%B2%D0%B5%D1%80%D1%88%D0%B0%D0%B5%D1%82-%D1%84%D1%83%D0%BD%D0%BA%D1%86%D0%B8%D1%8E-%D0%BF%D1%80%D0%B8-%D0%B2%D1%8B%D0%B7%D0%BE%D0%B2%D0%B5-cancel
+    
+    /*
+     A dispatch work item has a cancel flag. If it is cancelled before running, the dispatch queue wont execute it and will skip it. If it is cancelled during its execution, the cancel property return True. In that case, we can abort the execution
+     */
     
     var block: DispatchWorkItem?
     
